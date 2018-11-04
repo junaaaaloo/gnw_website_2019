7e410475e8eb96de1d5dfce01353c51b0b30b1eb
ivy <robinnico.purple@gmail.com>
2018-11-04 20:27:45 +0800

    test

--- app/Http/Controllers/ManageRegIDController.php
+++ app/Http/Controllers/ManageRegIDController.php
@@ -19,20 +19,20 @@ class ManageRegIDController extends Controller
         $user = Auth::user();
         $title = "Home";
 
-        if($user->hasRole('subscriber')) {
-            return redirect()->route('home');
-        } else if ($user->hasRole('administrator') || $user->hasRole('editor')) {
+//        if($user->hasRole('subscriber')) {
+//            return redirect()->route('home');
+//        } else if ($user->hasRole('administrator') || $user->hasRole('editor')) {
         	$title = "Manage Registered IDs";
             $regIds = RegisteredIds::all();
 
             $role = "Administrator";
-            if($user->hasRole('editor')) {
-                $role = "Editor";
-            }
+//            if($user->hasRole('editor')) {
+//                $role = "Editor";
+//            }
 
             return view('admin/manageid', 
                 ['title' => $title, 'role' => $role, 'regIds' => $regIds]);
-        }
+//        }
     }
 
     public function create(Request $request) {
